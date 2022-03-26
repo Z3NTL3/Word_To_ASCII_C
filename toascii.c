1a1515b78b1e18d466c9f7782456ecb790dd075f
z3ntl3 <48758770+Z3NTL3@users.noreply.github.com>
2022-03-26 14:19:02 +0100

    Update toascii.c

--- toascii.c
+++ toascii.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 char ToASCII(char* woordToASCII){
     
     /* Some layout */
@@ -22,14 +23,22 @@ char ToASCII(char* woordToASCII){
     
 }
 
-
 int main()
 {
-    char w;
-    char* woord = (char*)calloc(10, sizeof(char));    
+    
+    char* lijn1 = "╔═╗╔═╗╔═╗╦╦  ╔╦╗╔═╗╔╗ ╦  ╔═╗\n";
+    char* lijn2 = "╠═╣╚═╗║  ║║   ║ ╠═╣╠╩╗║  ║╣ \n";
+    char* lijn3 = "╩ ╩╚═╝╚═╝╩╩   ╩ ╩ ╩╚═╝╩═╝╚═╝\n";
+    char* asciilogo = (char*)calloc(strlen(lijn1)*3,sizeof(char));
+    asciilogo = strcat(asciilogo, lijn1);
+    asciilogo = strcat(asciilogo, lijn2);
+    asciilogo = strcat(asciilogo, lijn3);
+
+    char* woord = (char*)calloc(10, sizeof(char)); 
+    printf("%s", asciilogo);
 
     printf("Write some letters: ");
-    scanf("%s", &*woord);
+    scanf("%s", woord);
     printf("\n");
     
     if(strlen(woord) >= 10){
@@ -42,6 +51,9 @@ int main()
     
     printf("You have entered: %s\n", woord);
     ToASCII(woord);
+    free(woord);
     printf("\n");
+    
     return 0;
+    
 }
